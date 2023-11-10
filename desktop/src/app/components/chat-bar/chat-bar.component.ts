36608a364878cf8b00cb0e425fc8057b01b3f48d
tetreaultmarc <43557279+tetreaultmarc@users.noreply.github.com>
2023-11-09 22:01:38 -0500

    change mongodb string

--- desktop/src/app/components/chat-bar/chat-bar.component.ts
+++ desktop/src/app/components/chat-bar/chat-bar.component.ts
@@ -1,24 +1,32 @@
-import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from "@angular/core";
 import { ElectronService } from "ngx-electron";
-import { ChatService } from '@app/services/chat/chat.service'
-import { FormBuilder, Validators } from '@angular/forms';
-import { SocketService } from '@app/services/socket/socket.service';
-import { GameService } from '@app/services/game/game.service';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { ChatService } from "@app/services/chat/chat.service";
+import { FormBuilder, Validators } from "@angular/forms";
+import { SocketService } from "@app/services/socket/socket.service";
+import { GameService } from "@app/services/game/game.service";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-chat-bar',
-  templateUrl: './chat-bar.component.html',
-  styleUrls: ['./chat-bar.component.scss']
+  selector: "app-chat-bar",
+  templateUrl: "./chat-bar.component.html",
+  styleUrls: ["./chat-bar.component.scss"],
 })
-
 export class ChatBarComponent implements OnInit, OnDestroy {
   isWindowButtonAvailable: boolean = true;
 
-  constructor(private snackBar: MatSnackBar, private ngZone: NgZone, private router: Router, public chatService: ChatService, private electronService: ElectronService, private formBuilder: FormBuilder, private socketService: SocketService, public gameService: GameService) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private ngZone: NgZone,
+    private router: Router,
+    public chatService: ChatService,
+    private electronService: ElectronService,
+    private formBuilder: FormBuilder,
+    private socketService: SocketService,
+    public gameService: GameService
+  ) {}
   createChatForm = this.formBuilder.group({
-    chatName: ['', Validators.maxLength(12)],
+    chatName: ["", Validators.maxLength(12)],
   });
 
   changeChat(id: string): void {
@@ -26,85 +34,103 @@ export class ChatBarComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    if(!this.electronService.process){
+    if (!this.electronService.process) {
       this.isWindowButtonAvailable = false;
     }
   }
 
   ngOnDestroy(): void {
-    if(this.chatService.isChatInExternalWindow){
-      localStorage.removeItem('joinedChats');
-      localStorage.removeItem('notJoinedChats');
-      localStorage.removeItem('currentChatId');      
+    if (this.chatService.isChatInExternalWindow) {
+      localStorage.removeItem("joinedChats");
+      localStorage.removeItem("notJoinedChats");
+      localStorage.removeItem("currentChatId");
     }
   }
 
   openExternalWindow(): void {
-    this.socketService.bind('openExternalChatCallback', (data: any) => {
-      this.socketService.unbind('openExternalChatCallback');
-      this.socketService.emit('openExternalChat', {linkedSocketId: data.externalWindowSocketid});
+    this.socketService.bind("openExternalChatCallback", (data: any) => {
+      this.socketService.unbind("openExternalChatCallback");
+      this.socketService.emit("openExternalChat", {
+        linkedSocketId: data.externalWindowSocketid,
+      });
     });
-    localStorage.setItem('joinedChats', JSON.stringify(this.chatService.joinedChatList));
-    localStorage.setItem('notJoinedChats', JSON.stringify(this.chatService.notJoinedChatList));
-    localStorage.setItem('socketId' , this.socketService.socket.id);
-    localStorage.setItem('isExternalWindow', 'true');
-    localStorage.setItem('currentChatId', this.chatService.currentChatId);
+    localStorage.setItem(
+      "joinedChats",
+      JSON.stringify(this.chatService.joinedChatList)
+    );
+    localStorage.setItem(
+      "notJoinedChats",
+      JSON.stringify(this.chatService.notJoinedChatList)
+    );
+    localStorage.setItem("socketId", this.socketService.socket.id);
+    localStorage.setItem("isExternalWindow", "true");
+    localStorage.setItem("currentChatId", this.chatService.currentChatId);
     this.chatService.isChatInExternalWindow = true;
-    let BrowserWindow = this.electronService.remote.BrowserWindow
+    let BrowserWindow = this.electronService.remote.BrowserWindow;
     this.gameService.chatWindow = new BrowserWindow({
       width: 384,
       height: 840,
       resizable: false,
-    })
-    this.gameService.chatWindow.loadURL('file://' + __dirname + '/index.html#/chat');
-    let chatBar = document.getElementById('chat-bar');
-    if(chatBar){
-      chatBar.style.display = 'none';
+    });
+    this.gameService.chatWindow.loadURL(
+      "file://" + __dirname + "/index.html#/chat"
+    );
+    let chatBar = document.getElementById("chat-bar");
+    if (chatBar) {
+      chatBar.style.display = "none";
     }
-    this.gameService.chatWindow.on('close', () => {
-      this.socketService.emit('closeExternalChat', {mainWindowId: this.socketService.socketId});
-      localStorage.removeItem('joinedChats');
-      localStorage.removeItem('socketId');
+    this.gameService.chatWindow.on("close", () => {
+      this.socketService.emit("closeExternalChat", {
+        mainWindowId: this.socketService.socketId,
+      });
+      localStorage.removeItem("joinedChats");
+      localStorage.removeItem("socketId");
       this.chatService.isChatInExternalWindow = false;
-      let chatBar = document.getElementById('chat-bar');
-      if(chatBar){
-        chatBar.style.display = 'block';
+      let chatBar = document.getElementById("chat-bar");
+      if (chatBar) {
+        chatBar.style.display = "block";
       }
-      this.chatService.isClosingExternalWindow = true; 
+      this.chatService.isClosingExternalWindow = true;
       let currentUrl = this.router.url;
       this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-      this.router.onSameUrlNavigation = 'reload';
-      this.ngZone.run(() => this.router.navigate([currentUrl]))
-    })
+      this.router.onSameUrlNavigation = "reload";
+      this.ngZone.run(() => this.router.navigate([currentUrl]));
+    });
   }
 
-  
   createChat(): void {
-    if(this.createChatForm.value.chatName == "" || !this.createChatForm.value.chatName) return;
-    if(this.createChatForm.valid) {
+    if (
+      this.createChatForm.value.chatName == "" ||
+      !this.createChatForm.value.chatName
+    )
+      return;
+    if (this.createChatForm.valid) {
       this.chatService.createChat(this.createChatForm.value.chatName);
     } else {
-      this.snackBar.open("Le nom du canal doit être en 1 et 12 charactères!", "", {
-        duration: 2000,
-      });
+      this.snackBar.open(
+        "Le nom du canal doit être en 1 et 12 charactères!",
+        "",
+        {
+          duration: 2000,
+        }
+      );
     }
     this.createChatForm.reset();
-
   }
-  
+
   joinChat(chatId: string): void {
-    this.socketService.bind('joinChatRoomCallback', () => {
+    this.socketService.bind("joinChatRoomCallback", () => {
       this.chatService.refreshChatList();
-      this.socketService.unbind('joinChatRoomCallback')
+      this.socketService.unbind("joinChatRoomCallback");
     });
     this.chatService.joinChat(chatId);
   }
 
   leaveChat(chatId: string): void {
-    this.socketService.bind('leaveChatRoomCallback', () => {
+    this.socketService.bind("leaveChatRoomCallback", () => {
       this.chatService.refreshChatList();
-      this.socketService.unbind('leaveChatRoomCallback');
-      this.chatService.setCurrentChat('General');
+      this.socketService.unbind("leaveChatRoomCallback");
+      this.chatService.setCurrentChat("General");
     });
     this.chatService.leaveChat(chatId);
   }
@@ -115,8 +141,8 @@ export class ChatBarComponent implements OnInit, OnDestroy {
 
   isGameChat(): boolean {
     try {
-      if(this.chatService.joinedChatList[this.chatService.index].isGameChat){
-        return true
+      if (this.chatService.joinedChatList[this.chatService.index].isGameChat) {
+        return true;
       } else {
         return false;
       }
--- server/app/models/database.model.ts
+++ server/app/models/database.model.ts
@@ -1,25 +1,34 @@
-import { injectable } from 'inversify';
-import { MongoClient }from 'mongodb';
+import { injectable } from "inversify";
+import { MongoClient } from "mongodb";
 
 @injectable()
 export class DatabaseModel {
-    mongoURL: string = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@cluster0.oia6z.mongodb.net/database?retryWrites=true&w=majority";
-    client: MongoClient = new MongoClient(this.mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
-    private static instance : DatabaseModel;
+  mongoURL: string =
+    "mongodb+srv://" +
+    process.env.MONGO_USERNAME +
+    ":" +
+    process.env.MONGO_PASSWORD +
+    "@thegenerativeproject.ekuhi2e.mongodb.net/?retryWrites=true&w=majority";
 
-    public static getInstance(): DatabaseModel {
-        if (!DatabaseModel.instance) {
-            DatabaseModel.instance = new DatabaseModel();
-        }
-        return DatabaseModel.instance;
-    }
+  client: MongoClient = new MongoClient(this.mongoURL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  private static instance: DatabaseModel;
 
-    async startDB() {
-        try {
-            await this.client.connect();
-        } catch (e) {
-            console.error(e);
-        }
-        console.log("We are on the moon");
+  public static getInstance(): DatabaseModel {
+    if (!DatabaseModel.instance) {
+      DatabaseModel.instance = new DatabaseModel();
     }
+    return DatabaseModel.instance;
+  }
+
+  async startDB() {
+    try {
+      await this.client.connect();
+    } catch (e) {
+      console.error(e);
+    }
+    console.log("We are on the moon");
+  }
 }
