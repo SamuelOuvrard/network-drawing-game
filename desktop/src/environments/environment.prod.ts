adfa8ed748a4f536a419d689edb7507a9c3a875f
tetreaultmarc <43557279+tetreaultmarc@users.noreply.github.com>
2023-12-05 20:52:39 -0500

    adds the new ip address to prod conf for desktop

--- desktop/src/environments/environment.prod.ts
+++ desktop/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    api_url: "http://18.219.29.27:3000",
-    socket_url: "http://18.219.29.27:3000/",
+  production: true,
+  api_url: "http://18.117.193.47:3000",
+  socket_url: "http://18.117.193.47:3000/",
 };
